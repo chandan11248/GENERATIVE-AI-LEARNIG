ae09b37c2e01cfb33b693283ac29445231bdea35
chandan shah <letschandansah@gmail.com>
2025-11-27 15:31:23 +0545

    ..

--- pydanticPractice.py
+++ pydanticPractice.py
@@ -2,15 +2,24 @@ from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 load_dotenv()
+from pydantic import BaseModel,Field
+from typing import Optional, Literal
 
-model = ChatGroq(model="llama-3.3-70b-versatile")   # FIXED
-
+model = ChatGroq(model="llama-3.3-70b-versatile",temperature=1.2)  
 template = PromptTemplate(
     template="tell me the name of a random {types} guy.",
     input_variables=['types'],
 )
 
+class infos (BaseModel):
+    name:str
+    age:int=Field(gt=0 ,lt=35,description=("the age of the person"))
+
+
+structured_model=model.with_structured_output(infos)
+
+
 prompt = template.format(types="asian")   # FIXED format usage
 
-result = model.invoke(prompt)
-print(result.content)
+result = structured_model.invoke(prompt)
+print(result)
